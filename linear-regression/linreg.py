e4c9e63ecbae8a36c3d631fed8b2724d4ffb19ee
zkan <kan@prontomarketing.com>
2017-10-01 19:07:12 +0700

    Add linear regression in Python

---
+++ linear-regression/linreg.py
@@ -0,0 +1,59 @@
+class LinearRegression:
+    def dot_product(self, a, b):
+        if len(a) != len(b):
+            return -1
+
+        result = 0
+        for each in xrange(len(a)):
+            result += a[each] * b[each]
+
+        return result
+
+    def compute_cost(self, X, y, theta):
+        J = 0
+        predictions = []
+
+        for each in X:
+            predictions.append(self.dot_product(each, theta))
+
+        errors = []
+        for index, each in enumerate(predictions):
+            errors.append(each - y[index])
+
+        J = (1.0 / 2.0) * self.dot_product(errors, errors)
+
+        return J
+
+    def run_gradient_descent(self, X, y, theta, number_of_iterations):
+        alpha = 0.01
+        J_history = []
+
+        for each_iteration in xrange(number_of_iterations):
+            predictions = []
+            for each_example in X:
+                predictions.append(self.dot_product(each_example, theta))
+
+            for t_index, each_theta in enumerate(theta):
+                diff = []
+                X_col = []
+                for p_index, each_prediction in enumerate(predictions):
+                    diff.append(each_prediction - y[p_index])
+                    X_col.append(X[t_index][p_index])
+
+                error = self.dot_product(diff, X_col)
+                result = each_theta - (alpha * error)
+                theta[t_index] = result
+
+            J = self.compute_cost(X, y, theta)
+            J_history.append(J)
+
+        return (J_history, theta)
+
+
+linreg = LinearRegression()
+X = [[1, 2], [3, 4]]
+y = [2, 2.5]
+theta = [0, 0]
+number_of_iterations = 15
+results = linreg.run_gradient_descent(X, y, theta, number_of_iterations)
+print(results)
